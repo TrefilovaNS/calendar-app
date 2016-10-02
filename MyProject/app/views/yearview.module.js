4bf6948946e8348e34ba9a3c07c5ccd75c9740d6
TrefilovaNS <mionavlika@ymail.com>
2016-10-03 00:01:29 +0300

    YearView in progress

--- MyProject/app/views/yearview.module.js
+++ MyProject/app/views/yearview.module.js
@@ -8,30 +8,72 @@ app.controller('YearController', function YearController($scope) {
 	
   $scope.options = $scope.options || {};
   $scope.options.defaultDate = new Date ();
+  $scope.nextYear = nextYear;
+  $scope.resetToToday = resetToToday;
 
 
    var MONTHS = ['JANUARY', 'FEBRUARY', 'MARCH', 'APRIL', 'MAY', 'JUNE', 'JULY', 'AUGUST', 'SEPTEMBER', 'OCTOBER', 'NOVEMBER', 'DECEMBER'];
     var WEEKDAYS = ['MONDAY' , 'TUESDAY' , 'WEDNESDAY' , 'THURSDAY' , 'FRIDAY' , 'SATURDAY', 'SUNDAY'];
+     
      $scope.$watch('options.defaultDate', function() {
       calculateSelectedDate();
     });
 
+    //   $scope.$watch('selectedYear', function() {
+    //     $scope.$broadcast('selectedYear');
+    //     console.log('Hi!');
+    // });
 
-
+  
 
    function calculateSelectedDate() {
-      if ($scope.options.defaultDate) {
-        $scope.options._defaultDate = new Date($scope.options.defaultDate);
-      } else {
-        $scope.options._defaultDate = new Date();
-      }
 
-      $scope.selectedYear  = $scope.options._defaultDate.getFullYear();
-      $scope.selectedMonth = MONTHS[$scope.options._defaultDate.getMonth()];
-      $scope.selectedDay   = $scope.options._defaultDate.getDate();
+      $scope.selectedYear  = $scope.options.defaultDate.getFullYear();
+      $scope.selectedMonth = MONTHS[$scope.options.defaultDate.getMonth()];
+      $scope.selectedDay   = $scope.options.defaultDate.getDate();
     }
 
+    function nextYear() {
+      
+        $scope.selectedYear += 1;
+      //  var year = $scope.selectedYear;
+     //   $scope.$broadcast('Year', year);
 
+            
+    }
+
+     function resetToToday() {
+        $scope.options.defaultDate = new Date();
+        $scope.selectedYear  = $scope.options.defaultDate.getFullYear();
+       
+    }
 });
 
+app.controller('JanController', function JanController($scope) {
+   
+   // $scope.$watch('Year', function() {
+   //   // $scope.selectedYear = args;
+   //   //  calculate();
+   //   console.log("Hello!")
+   //  });
+
+  calculate();
+
+  function calculate(){
+    $scope.options.defaultDate = new Date();
+    $scope.selectedYear  = $scope.options.defaultDate.getFullYear();
+    $scope.options.defaultDate = new Date($scope.selectedYear,0,1);
+    
+  }
+    
+   
+});
+
+app.controller('FebController', function FebController($scope) {
+    
+    $scope.options.defaultDate = new Date();
+    $scope.selectedYear  = $scope.options.defaultDate.getFullYear();
+    $scope.options.defaultDate = new Date($scope.selectedYear,1,1);
+    
+});
 //для каждого месяца - свой контроллер
