b20bf82660cef5b8828073a5ef39a0ed41e9a8b5
TrefilovaNS <mionavlika@ymail.com>
2017-02-08 10:57:19 +0300

    Added new tests

--- MyProject/e2e-tests/scenarios.js
+++ MyProject/e2e-tests/scenarios.js
@@ -8,6 +8,7 @@ describe('Calendar Application', function() {
 
      beforeEach(function() {
       browser.get('index.html');
+      
     });
     // beforeEach(module('calendarApp'));
     
@@ -17,8 +18,60 @@ describe('Calendar Application', function() {
     expect(element(by.css('.navbar-brand')).getText()).toEqual('Calendar App');
  });
 
-    // it('should have a title', function() {
-    // browser.get('http://localhost:8000/#/events');
+it('should click on Views and then Month', function() {
+    var openViews = browser.findElement(By.xpath("//a[@href='#views']"));
+    openViews.click();
+
+    var tabs = element.all(by.css('.nav-tabs li'));
+    tabs.get(2).click();
+
+    // var next = element(by.css('.month div.arrow'));
+    // var EC = protractor.ExpectedConditions;
+    // browser.wait(EC.visibilityOf(next), 5000);
+    // next.click();
+ });
+
+it('should click on date picker', function() {
+  var startDate = browser.findElement(By.id("startDate"));
+  //var select = browser.findElement(By.xpath("html/body/div[3]/div[1]/table/tbody/tr[2]/td[3]")); 
+   startDate.click();
+    setTimeout(function(){
+         
+    }, 5000);  
+   //select.click()
+    //  var openViews = browser.findElement(By.xpath("//th[@text='#views']"));
+
+    // /html/body/div[3]/div[1]/table/tbody/tr[2]/td[3]
+ });
+
+it('should click on name and type smth, save and check', function() {
+  var name = browser.findElement(By.id("name"));
+  var description = browser.findElement(By.id("description"));
+  var startDate = browser.findElement(By.id("startDate"));
+  var startTime = browser.findElement(By.id("startTime"));
+  var endTime = browser.findElement(By.id("endTime"));
+  var endDate = browser.findElement(By.id("endDate"));
+  var place = browser.findElement(By.id("place"));
+  var add = browser.findElement(By.id("addEvent"));
+  name.sendKeys("Events");
+  description.sendKeys("Happy day");
+  //Need to rewrite in click way
+  startDate.sendKeys("07-02-2017");
+  startTime.sendKeys("08:00");
+  endTime.sendKeys("18:00");
+  endDate.sendKeys("07-02-2017");
+  place.sendKeys("Home");
+    add.click();
+ var findNameInList = browser.findElement(By.xpath("//*[@id='list']/tr/td[2]"));
+ 
+ });
+
+// it('should click on Month', function() {
+//     // var openMonth = browser.findElement(By.xpath("//a[@ng-click='tab.setTab(2)']"));
+//     // openMonth.click();
+
+//  });
+
 
     
     //   });
